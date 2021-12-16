eeb14cc35449121f84781589bd2b7648dc52ff4d
Kull Trần <kull>
2021-12-16 23:32:14 +0700

    push layout profile

---
+++ app/src/main/java/com/devapp/fr/ui/fragments/FragmentProfile.kt
@@ -0,0 +1,14 @@
+package com.devapp.fr.ui.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.devapp.fr.R
+import com.devapp.fr.databinding.FragmentLovesBinding
+
+class FragmentProfile : Fragment(R.layout.fragment_profile) {
+
+}
